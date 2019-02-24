7cf144794a12e9fed5a9d11208b4eb5b068a44ff
Ola <HP@OLA>
2019-02-24 17:24:14 +0100

    Add notification prev stuff part1

---
+++ MyDevLab/Controllers/WebPushController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyDevLab.ViewModels.WebPush;
+
+namespace MyDevLab.Controllers
+{
+    [Authorize]
+    public class WebPushController : Controller
+    {
+        private const string VIEW_MODELS_PATH = "~/Views/WebPush/";
+        private const string CSHTML_EXT = ".cshtml";
+
+
+        [HttpGet]
+        public IActionResult Wizard()
+        {
+            return View($"{VIEW_MODELS_PATH}WizardIndex{CSHTML_EXT}");
+        }
+
+        [HttpGet]
+        public IActionResult Statistics()
+        {
+            return View($"{VIEW_MODELS_PATH}StatisticsIndex{CSHTML_EXT}");
+        }
+
+        [HttpGet]
+        public IActionResult LivePreview()
+        {
+            var livePreview = new LivePreviewVM()
+            {
+                Subject = "Test Web Push Subject",
+                BigImageImg = "..\\images\\WebPush\\LivePreview\\grumpyCat.jpg",
+                Body = "Test body content",
+                IconImg = "..\\images\\WebPush\\LivePreview\\grumpyCatIcon.png",
+                IsAction1Selected = true,
+                Action1IconImg = "..\\images\\WebPush\\LivePreview\\yes.png",
+                Action1Title = "Action 1 Test",
+                IsAction2Selected = true,
+                Action2IconImg = "..\\images\\WebPush\\LivePreview\\no.png",
+                Action2Title = "Action 2 Test"
+            };
+
+            return View($"{VIEW_MODELS_PATH}LivePreview{CSHTML_EXT}", livePreview);
+        }
+    }
+}
---
+++ MyDevLab/ViewModels/WebPush/LivePreviewVM.cs
@@ -0,0 +1,16 @@
+﻿namespace MyDevLab.ViewModels.WebPush
+{
+    public class LivePreviewVM
+    {
+        public string BigImageImg { get; set; }
+        public string IconImg { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsAction1Selected { get; set; }
+        public bool IsAction2Selected { get; set; }
+        public string Action1IconImg { get; set; }
+        public string Action2IconImg { get; set; }
+        public string Action1Title { get; set; }
+        public string Action2Title { get; set; }
+    }
+}
